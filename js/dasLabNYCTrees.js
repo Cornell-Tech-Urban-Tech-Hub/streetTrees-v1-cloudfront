969ccc9058c86d208fbbbec0581725616fcb0ecc
Anthony Townsend <amt3@nyu.edu>
2022-05-16 15:53:10 -0400

    update paths again

--- js/dasLabNYCTrees.js
+++ js/dasLabNYCTrees.js
@@ -230,8 +230,7 @@ map.on('load', function() {
 
 	function shadow(zipcode,species,treeID,treeLat,treeLon,az,amp,darkness,name,bool) {
 
-		// var pointCloudFile = ' http://www.treefolio.org.s3-website-us-east-1.amazonaws.com/folio/folio/';
-		var pointCloudFile = ' /folio/folio/';
+		var pointCloudFile = ' ./folio/folio/';
 		var pointCloudFile = pointCloudFile.concat(zipcode);
 		var pointCloudFile = pointCloudFile.concat('/');
 		var pointCloudFile = pointCloudFile.concat(zipcode);
@@ -306,8 +305,7 @@ map.on('load', function() {
 		document.getElementById("common").innerHTML = species;
 
 		// POINT CLOUD FILE PATH GOES HERE ///////////////////////////////////////////////////////////////////////////////////////
-		// var pointCloudFile = ' http://www.treefolio.org.s3-website-us-east-1.amazonaws.com/folio/folio/';
-		var pointCloudFile = ' /folio/folio/';
+		var pointCloudFile = ' ./folio/folio/';
 		var pointCloudFile = pointCloudFile.concat(zipcode);
 		var pointCloudFile = pointCloudFile.concat('/');
 		var pointCloudFile = pointCloudFile.concat(zipcode);
